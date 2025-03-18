80bdff4114f92485fdfeaff32be901e144de3b2e
Sandro Seivane <seivanesandro@gmail.com>
2025-03-18 16:31:12 +0000

    Updates

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+// prettier.config.js or .prettierrc.js
+(module.exports = {
+        "semi": true,
+        "singleQuote": true,
+        "printWidth": 50,
+        "tabWidth": 4,
+        "trailingComma": "none",
+        "arrowParens": "avoid"
+});
+
+
+(module.exports = {
+    name: "my-first-library",
+    version: "9000.0.1",
+    prettier: "@futureminds/prettier-config",
+  });
